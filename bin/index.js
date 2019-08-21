4d57f4e0ce8f43e9236797ed7ee131e952bf2044
A-312 <A-312@users.noreply.github.com>
2019-08-21 22:08:24 +0200

    Check if the context is a terminal (prevent crash)

--- bin/index.js
+++ bin/index.js
@@ -61,10 +61,14 @@ function Serverline() {
       return rl
     },
     getHistory: function() {
-      return rl.history
+      return (rl.terminal) ? rl.history : []
     },
     setHistory: function(history) {
-      rl.history = history
+      if (rl.terminal && Array.isArray(history)) {
+        rl.history = history
+        return true
+      }
+      return !!rl.terminal
     },
     pause: function() {
       rl.pause()
@@ -114,6 +118,11 @@ function init(options) {
     prompt: slOptions.prompt
   })
 
+  if (!rl.terminal) {
+    console.warn('WARN: Compatibility mode! The current context is not a terminal. This may ' +
+      'occur when you redirect terminal output into a file.')
+  }
+
   const consoleOptions = {}
 
   ;(['colorMode', 'inspectOptions', 'ignoreErrors']).forEach((val) => {
@@ -126,15 +135,19 @@ function init(options) {
   hiddenOverwrite()
 
   rl.on('line', function(line) {
-    if (!stdoutMuted && rl.history) {
+    if (!stdoutMuted && rl.history && rl.terminal) {
       rl.history.push(line)
     }
     myEmitter.emit('line', line)
-    rl.prompt()
+    if (rl.terminal) {
+      rl.prompt()
+    }
   })
   rl.on('SIGINT', function() {
     fixSIGINTonQuestion = !!rl._questionCallback
-    rl.line = ''
+    if (rl.terminal) {
+      rl.line = ''
+    }
     if (!myEmitter.emit('SIGINT', rl)) {
       process.exit(0)
     }
@@ -155,7 +168,9 @@ function secret(query, callback) {
   const toggleAfterAnswer = !stdoutMuted
   stdoutMuted = true
   rl.question(query, function(value) {
-    rl.history = rl.history.slice(1)
+    if (rl.terminal) {
+      rl.history = rl.history.slice(1)
+    }
 
     if (toggleAfterAnswer) {
       stdoutMuted = false
@@ -170,14 +185,14 @@ function hiddenOverwrite() {
     // https://github.com/nodejs/node/blob/v10.0.0/lib/readline.js#L326 && ./v9.5.0/lib/readline.js#L335
     return function _refreshLine() {
       let abc
-      if (stdoutMuted) {
+      if (stdoutMuted && rl.line) {
         abc = rl.line
         rl.line = ''
       }
 
       refresh.call(rl)
 
-      if (stdoutMuted) {
+      if (stdoutMuted && rl.line) {
         rl.line = abc
       }
     }
@@ -188,8 +203,12 @@ function hiddenOverwrite() {
     return function _writeToOutput(argStringToWrite) {
       let stringToWrite = argStringToWrite
 
-      if (stdoutMuted) {
+      if (!stdoutMuted) {
+        stringToWrite = argStringToWrite
+      } else if (rl.terminal) { // muted && terminal
         stringToWrite = '\x1B[2K\x1B[200D' + rl._prompt + '[' + ((rl.line.length % 2 === 1) ? '=-' : '-=') + ']'
+      } else { // muted && terminal == false
+        stringToWrite = ''
       }
 
       write.call(rl, stringToWrite)
@@ -205,10 +224,15 @@ function consoleOverwrite(options) {
 
   Object.keys(collection).forEach((name) => {
     collection[name]._write = function(chunk, encoding, callback) {
-      original[name].write(beforeTheLastLine(chunk), encoding, () => {
-        rl._refreshLine()
-        callback()
-      })
+      // https://github.com/nodejs/node/blob/v10.0.0/lib/readline.js#L178
+      if (rl.terminal) {
+        original[name].write(beforeTheLastLine(chunk), encoding, () => {
+          rl._refreshLine()
+          callback()
+        })
+      } else {
+        original[name].write(chunk, encoding, callback)
+      }
     }
   })
 
